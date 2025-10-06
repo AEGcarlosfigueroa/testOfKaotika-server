7f9c3bee0c507c9bdb950b10b5875a02c1d1a3e9
Carlos Aguilar <carlos.figueroa@ikasle.aeg.eus>
2025-10-06 13:48:28 +0200

    [ADD] basic socket.io server

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose')
 const mongodbRoute = process.env.MONGODB_URI;
 const morgan = require('morgan');
-const { verifyFirebaseToken } = require('./middlewares/verifyData')
-
+const { verifyFirebaseToken } = require('./middlewares/verifyData');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
 const usersRouter = require('./routes/userRoutes');
-
 
 const app = express();
 
@@ -16,11 +16,33 @@ app.use(morgan('dev'))
 
 const PORT = process.env.PORT || 3000;
 
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["https://amritb.github.io"] // FOR TESTING PURPOSES ONLY
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected with socket " + socket.id);
+  socket.on("info", (email) => {
+    console.log("email: " + email);
+  })
+});
+
+io.engine.on("connection_error", (err) => {
+  console.log(err.req);      // the request object
+  console.log(err.code);     // the error code, for example 1
+  console.log(err.message);  // the error message, for example "Session ID unknown"
+  console.log(err.context);  // some additional error context
+});
+
+httpServer.listen(PORT);
 
 app.use(bodyParser.json());
 
 app.use("/api/players", verifyFirebaseToken, usersRouter);     // For your MongoDB players
-
 
 async function start(){
     try

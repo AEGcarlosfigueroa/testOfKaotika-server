d921a79896449bfd10d0525cbc7a322ec820a560
AEGcarlosfigueroa <carlos.figueroa@ikasle.aeg.eus>
2025-10-08 15:36:34 +0200

    [REMOVE] node_modules, built, [ADD] node_modules and built into gitignore

--- built/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import * as express from "express";
-const usersRouter = express.Router();
-import * as userController from "../controllers/userController.ts";
-// Public route — anyone can access
-// router.get("/", verifyFirebaseToken, userController.getAllUsers);
-usersRouter.get("/email/:playerEmail", userController.getPlayerFromDatabaseByEmail);
-// Protected route — only users with valid ID token and verified email
-// router.post("/", verifyFirebaseToken, userController.getAllUsers);
-export { usersRouter };

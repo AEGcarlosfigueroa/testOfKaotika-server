a693cdbca521a616c08eccce202a7ffc8ea8118a
Carlos Aguilar <carlos.figueroa@ikasle.aeg.eus>
2025-11-06 09:23:35 +0100

    [ADD] SCRUM-90 player is now switched the isInTower attribute properly ans sends the new player to the client

--- src/ioServer/events/isInTowerEntramceRequest.ts
+++ src/ioServer/events/isInTowerEntramceRequest.ts
@@ -3,69 +3,14 @@ import { pendingSockets } from '../listeners/isInTowerListener.ts';
 import * as userService from './../../services/userService.ts';
 import { mqttClient } from '../../mqtt/mqttManager.ts';
 
-export default function isInTowerEntranceRequest(socketID: string)
+export default async function isInTowerEntranceRequest(socketID: string)
 {
     console.log("Send isInTowerRequest to: " + socketID)
     try
     {
-        const socket = server.in(socketID);
+        const socket = await server.in(socketID).fetchSockets();
 
-        console.log(socket);
-
-        socket.emit("isInTowerEntranceRequest", " ");
-
-        socket.on("is the player at the gates?", async(isAtTheGates) => {
-            try
-            {
-                if(isAtTheGates)
-                {
-                    for(let i=0; i<pendingSockets.length; i++)
-                    {
-                        if(pendingSockets[i] === socket.id)
-                        {
-                            pendingSockets.splice(i, 1);
-                            break;
-                        }
-                    }
-
-                    const player = await userService.getPlayerFromDatabaseBySocketId(socket.id);
-
-                    let isInTower = player.isInTower;
-
-                    isInTower = !isInTower;
-
-                    player.isInTower = isInTower;
-
-                    await player.save();
-
-                    socket.emit("authorization", player);
-
-                    mqttClient.publish('authorization', true, { qos: 0, retain: false }, (error) => {
-                      if (error) {
-                        console.error(error)
-                      }
-                    });
-                    console.log("Received response: true");
-                }
-                else
-                {
-                    const player = await userService.getPlayerFromDatabaseBySocketId(socket.id);
-
-                    socket.emit("authorization", player);
-
-                    mqttClient.publish('authorization', false, { qos: 0, retain: false }, (error) => {
-                      if (error) {
-                        console.error(error)
-                      }
-                    });
-                    console.log("Received response: false");
-                }
-            }
-            catch(error)
-            {
-                console.error(error);
-            }
-        })
+        socket[0].emit("isInTowerEntranceRequest", " ");
     }
     catch(error)
     {
--- src/ioServer/listeners/isInTowerListener.ts
+++ src/ioServer/listeners/isInTowerListener.ts
@@ -1,5 +1,7 @@
 import * as userService from './../../services/userService.ts'
 
+import { mqttClient } from '../../mqtt/mqttManager.ts';
+
 export const pendingSockets = [];
 
 export function isInTowerListener(socket: Socket, io: Server)
@@ -9,6 +11,8 @@ export function isInTowerListener(socket: Socket, io: Server)
         {
             console.log("isInTower triggered");
             let isInPendingList = false;
+
+            let authorizationToPublish = false;
 
             for(let i=0; i<pendingSockets.length; i++)
             {
@@ -22,19 +26,29 @@ export function isInTowerListener(socket: Socket, io: Server)
 
             const player = await userService.getPlayerFromDatabaseBySocketId(socket.id);
 
-            if(isInPendingList && player)
+            if(isInPendingList && player && isInTower)
             {
-                let inside = player.isInside;
+                let isInTower = player.isInTower;
 
-                inside = !inside;
+                isInTower = !isInTower;
 
-                player.isInside = inside;
+                player.isInTower = isInTower
 
                 await player.save();
 
                 socket.emit("authorization", player);
 
+                authorizationToPublish = true;
             }
+            
+            mqttClient.publish('authorization', `${authorizationToPublish}`, { qos: 0, retain: false }, (error) => {
+              if (error) {
+                console.error(error)
+              }
+            });
+
+            console.log("Player authorized: " + isInTower);
+            console.log("Current player status: " + player.isInTower);
 
         }
         catch(error)

3555080e902310aad11c8097cc526b854c61234d
Carlos Aguilar <carlos.figueroa@ikasle.aeg.eus>
2025-12-09 08:28:22 +0100

    [ADD] SCRUM-177 unsaved modifications meant to be pushed last commit

---
+++ src/ioServer/listeners/showArtifactsListener.ts
@@ -0,0 +1,65 @@
+import {Server, Socket} from 'socket.io';
+import * as userService from './../../services/userService.ts';
+import * as artifactService from './../../services/artifactService.ts';
+import { roles } from '../../database/playerRoles';
+import { server } from '../ioServer';
+import { obituaryStateList, states } from '../../globalVariables';
+
+export function showArtifactsListener(io: Server, socket: Socket)
+{
+    socket.on("showArtifacts", async() => {
+        try
+        {
+            console.log("show artifacts triggered");
+
+            const players = await userService.getAllUsers();
+
+            const artifacts = await artifactService.getAllArtifacts();
+
+            //Check if all acolytes and mortimer are connected
+            for(let i=0; i<players.length; i++)
+            {
+                const entry = players[i];
+
+                if(entry.profile.role === 'MORTIMER' || entry.profile.role === 'ACOLITO')
+                {
+                    if(entry.socketId === null || !entry.isInHallOfSages)
+                    {
+                        console.log("Mortimer or an acolyte are not in hall of sages, aboting");
+                        return;
+                    }
+                }
+            }
+
+            for(let i=0; i<artifacts.length; i++)
+            {
+                const entry = artifacts[i];
+
+                if(!entry.isCollected)
+                {
+                    console.log("At least one artifacts has not been collected, aborting...");
+                    return;
+                }
+            }
+
+            console.log("all conditions are met, sending emit to mortimer and acolytes...");
+
+            for(let i=0; i<players.length; i++)
+            {
+                const entry = players[i];
+
+                if(entry.profile.role === 'MORTIMER' || entry.profile.role === 'ACOLITO')
+                {
+                    server.in(entry.socketId).emit("artifactEvaluating", "");
+                }
+            }
+
+            states.obituaryState = obituaryStateList.evaluating;
+        }
+        catch(error)
+        {
+            console.error(error);
+        }
+    })
+}
+

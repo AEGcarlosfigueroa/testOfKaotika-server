de0cf2e7be4deb2794f4ea3271f1af4f4b725937
Carlos Aguilar <carlos.figueroa@ikasle.aeg.eus>
2026-01-09 08:39:52 +0100

    [FIX] bug where turnIntoBetrayer callback function crashed the server due to not importing userService

--- src/ioServer/listeners/turnIntoBetrayerListener.ts
+++ src/ioServer/listeners/turnIntoBetrayerListener.ts
@@ -1,21 +1,30 @@
 import {Server, Socket} from 'socket.io';
 import playerListUpdate from '../events/playerListUpdate.ts';
+import * as userService from "./../../services/userService.ts";
 
 export function turnIntoBetrayerListener(io: Server, socket: Socket)
 {
     socket.on("turnIntoBetrayer", async () => {
-        
-        const player = await userService.getPlayerFromDatabaseBySocketId(socket.id);
-
-        if(player.profile.role === 'ACOLITO' && !player.isBetrayer)
+        try
         {
-            player.isBetrayer = true; //A Kaotika API request will be made here when we have access to it
-
-            await player.save();
-
-            socket.emit("authorization", player);
-
-            playerListUpdate();
+            console.log("tuen into betrayer triggered");
+            const player = await userService.getPlayerFromDatabaseBySocketId(socket.id);
+            
+            if(player.profile.role === 'ACOLITO' && !player.isBetrayer)
+            {
+                player.isBetrayer = true; //A Kaotika API request will be made here when we have access to it
+            
+                await player.save();
+            
+                socket.emit("authorization", player);
+            
+                playerListUpdate();
+            }
         }
+        catch(error)
+        {
+            console.log(error);
+        }
+        
     })
 }

cf8851c493c1d2de1b63bd160e07c293efefaae0
Carlos Aguilar <carlos.figueroa@ikasle.aeg.eus>
2026-01-14 10:32:08 +0100

    [FIX] some problems caused by merge

--- src/statusTools/applyStatusEffect.ts
+++ src/statusTools/applyStatusEffect.ts
@@ -14,12 +14,13 @@ export async function ApplyStatusEffect(player: any, diseaseApplied: string) {
 export function reapplyStatusEffects(player: any, dbPlayer: any) {
 
     for(let i=0; i<dbPlayer.statusEffects.length; i++)
-    {
+        {
         const diseaseApplied = dbPlayer.statusEffects[i];
         if (diseaseApplied === deadlyEffects.ethaziumCurse) {
             player.attributes.strength *= 0.6;
             player.attributes.dexterity *= 0.6;
             player.attributes.intelligence *= 0.6;
+            player.attributes.constitution *= 0.6;
         }
         else if(diseaseApplied === deadlyEffects.medulaApocalypse){
             player.attributes.constitution *= 0.7;

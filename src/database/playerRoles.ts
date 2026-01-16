babcd83538272d2730ffd4c5ad699b9ed101a820
Carlos <carlos012100@gmail.com>
2026-01-16 09:15:43 +0100

    change roles for villian

--- src/database/playerRoles.ts
+++ src/database/playerRoles.ts
@@ -1,28 +1,36 @@
 export const playerRole = {
 
-    "classcraft.daw2@aeg.eus" : "ISTVAN",
-    "ignacio.ayaso@ikasle.aeg.eus" : "MORTIMER",
-    // "oskar.calvo@aeg.eus" : "MORTIMER",
-    "carlos.palacio@ikasle.aeg.eus" : "VILLANO",
-    
+  // "classcraft.daw2@aeg.eus" : "ISTVAN",
+  // "oskar.calvo@aeg.eus" : "MORTIMER",
+  "carlos.figueroa@ikasle.aeg.eus": "ISTVAN",
+  // "ozarate@aeg.eus": "VILLANO",
+
+  "carlos.palacio@ikasle.aeg.eus": "VILLANO",
+
+  "oskar.calvo@aeg.eus ": "MORTIMER",
 
 }
 
 export const roles = {
-    istvan: "classcraft.daw2@aeg.eus",
-    // villano: "ozarate@aeg.eus",
-    villano: "carlos.palacio@ikasle.aeg.eus",
-    // mortimer: "oskar.calvo@aeg.eus",
-    mortimer: "ignacio.ayaso@ikasle.aeg.eus"
+  istvan: "carlos.figueroa@ikasle.aeg.eus",
+  // villano: "ozarate@aeg.eus",
+  mortimer: "oskar.calvo@aeg.eus",
+  villano: "carlos.palacio@ikasle.aeg.eus",
+
 }
 const DEFAULT = "ACOLITO" //this is for students 
 export function getRoleByEmail(playerEmail: string) {
-  if (playerEmail === roles.istvan) return 'ISTVAN';
-
-  if (playerEmail === roles.mortimer) return 'MORTIMER';
-
-  if (playerEmail === roles.villano) return 'VILLANO';
-
-  return DEFAULT;
+  if (playerEmail === roles.istvan) {
+    return 'ISTVAN';
+  }
+  else if (playerEmail === roles.mortimer) {
+    return 'MORTIMER';
+  }
+  else if (playerEmail === roles.villano) {
+    return 'VILLANO';
+  }
+  else {
+    return 'ACOLITO'; // default for students
+  }
 }
 

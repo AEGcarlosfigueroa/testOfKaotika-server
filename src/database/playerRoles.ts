f9388e0ba22bdeeaca6b217887b35b3980be4487
Carlos Aguilar <carlos.figueroa@ikasle.aeg.eus>
2026-02-27 11:04:18 +0100

    change roles to default

--- src/database/playerRoles.ts
+++ src/database/playerRoles.ts
@@ -1,7 +1,7 @@
 export const playerRole = {
 
     "classcraft.daw2@aeg.eus" : "ISTVAN",
-    "eneko.azkue@ikasle.aeg.eus" : "MORTIMER",
+    "ignacio.ayaso@ikasle.aeg.eus" : "MORTIMER",
     // "oskar.calvo@aeg.eus" : "MORTIMER",
     "ozarate@aeg.eus" : "VILLANO",
     
@@ -13,7 +13,7 @@ export const roles = {
     villano: "ozarate@aeg.eus",
     // villano: "carlos.palacio@ikasle.aeg.eus",
     // mortimer: "oskar.calvo@aeg.eus",
-    mortimer: "eneko.azkue@ikasle.aeg.eus"
+    mortimer: "ignacio.ayaso@ikasle.aeg.eus"
 }
 const DEFAULT = "ACOLITO" //this is for students 
 export function getRoleByEmail(playerEmail: string) {

420d164e5b38e2210a8e7eb6aa4498cb1bb59ee1
Carlos Aguilar <carlos.figueroa@ikasle.aeg.eus>
2026-02-27 10:41:19 +0100

    change mortimer role

--- src/database/playerRoles.ts
+++ src/database/playerRoles.ts
@@ -1,8 +1,8 @@
 export const playerRole = {
 
     "classcraft.daw2@aeg.eus" : "ISTVAN",
-    // "eneko.azkue@ikasle.aeg.eus" : "MORTIMER",
-    "oskar.calvo@aeg.eus" : "MORTIMER",
+    "eneko.azkue@ikasle.aeg.eus" : "MORTIMER",
+    // "oskar.calvo@aeg.eus" : "MORTIMER",
     "ozarate@aeg.eus" : "VILLANO",
     
 
@@ -12,8 +12,8 @@ export const roles = {
     istvan: "classcraft.daw2@aeg.eus",
     villano: "ozarate@aeg.eus",
     // villano: "carlos.palacio@ikasle.aeg.eus",
-    mortimer: "oskar.calvo@aeg.eus",
-    // mortimer: "eneko.azkue@ikasle.aeg.eus"
+    // mortimer: "oskar.calvo@aeg.eus",
+    mortimer: "eneko.azkue@ikasle.aeg.eus"
 }
 const DEFAULT = "ACOLITO" //this is for students 
 export function getRoleByEmail(playerEmail: string) {
